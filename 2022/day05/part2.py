cad0cb680fec78357ffa12f9689854a4ce8079a2
Guillaume Lemay <guilemay@gmail.com>
2022-12-05 13:16:08 -0500

    Completed day 5.

---
+++ 2022/day05/part2.py
@@ -0,0 +1,22 @@
+import sys
+
+lines = [l.rstrip() for l in sys.stdin]
+
+grid = lines[:lines.index('')]
+
+stacks = [ [] for i in range(10) ]
+for g in range(len(grid)-2, -1, -1) :
+    for i in range(1, 34, 4):
+        l = grid[g].ljust(34, ' ')
+        if l[i] == ' ':
+            continue
+        stacks[i//4 +1].append(l[i])
+
+steps = lines[lines.index('')+1:]
+for s in steps:
+    _, nb, _, src, _, dst = s.split(' ')
+    nb, src, dst = int(nb), int(src), int(dst)
+    stacks[dst] += stacks[src][-nb:]
+    stacks[src] = stacks[src][:-nb]
+
+print(''.join([s[-1] if len(s)>0 else '' for s in stacks]))
